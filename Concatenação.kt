98d166de9569811680a19ac5726b1ba6ae51e772
lyasantoscode <147869869+lyasantoscode@users.noreply.github.com>
2024-11-12 20:30:34 -0400

    Concatenação.kt

---
+++ Concatenação.kt
@@ -0,0 +1,14 @@
+fun main (args: Array<String>) {
+
+  var nome = "Lya"
+  var idade = 26
+  var profissao = "Programador Android"
+
+
+  //java é assim println ("Meu nome é " + nome + "eu tenho " + idade + " anos e sou" + profissap)
+
+  println ("Meu nome é " $nome eu tenho $idade anos e sou $profissao")   
+
+           // kotlin concatenção acima
+   
+}
