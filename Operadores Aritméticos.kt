bdd8f636b65d2e8c39add4566e0a93ba5fa1e74d
lyasantoscode <147869869+lyasantoscode@users.noreply.github.com>
2024-11-12 20:42:38 -0400

    Operadores Aritméticos.kt

---
+++ Operadores Aritméticos.kt
@@ -0,0 +1,97 @@
+fun main (args: Arrays<String>) {
+
+var numero1 = 20
+var numero2 = 10
+
+
+  println (numero1 + numero2) // soma
+
+
+}
+
+==========================================
+
+fun main (args: Arrays<String>) {
+
+var numero1 = 20
+var numero2 = 10
+
+
+  println (numero1 - numero2) // subtração
+
+
+}
+
+==========================================
+
+fun main (args: Arrays<String>) {
+
+var numero1 = 20
+var numero2 = 10
+
+
+  println (numero1 / numero2) // divisão
+
+
+}
+
+==========================================
+
+fun main (args: Arrays<String>) {
+
+var numero1 = 20
+var numero2 = 10
+
+
+  println (numero1 * numero2) // multiplicação
+
+
+}
+
+==========================================
+
+fun main (args: Arrays<String>) {
+
+var numero1 = 20
+var numero2 = 10
+
+
+  println (numero1 % numero2) // resto da divisão
+
+
+}
+
+==========================================
+
+fun main (args: Arrays<String>) {
+
+var numero1 = 20
+var numero2 = 10
+
+
+  println (++numero1) // incremento  ++   resultado será 21.... que incrementa +1
+
+
+}
+
+==========================================
+
+fun main (args: Arrays<String>) {
+
+var numero1 = 20
+var numero2 = 10
+
+
+  println (--numero2) // decremento vai tirar -1, resultado 9
+
+
+}
+
+
+
+
+
+
+
+
+
