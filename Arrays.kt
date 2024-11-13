5d7e2ec57aff72a5d4085ec2a7b244cd12d238f2
lyasantoscode <147869869+lyasantoscode@users.noreply.github.com>
2024-11-12 23:24:43 -0400

    Arrays.kt

---
+++ Arrays.kt
@@ -0,0 +1,82 @@
+fun main (args: Array>String>) {
+
+var listaCompras = arrayOf ("Arroz", "Feijao", "Batata", "Uva", "Banana", "Carne", "Leite", "Uva")
+
+//lista uma por uma, vai pela posição do indice, 1, 2, 3, lembrando que o indice zero iniciar com Arroz
+
+  println(listaCompras[6])
+  
+}
+
+//resultado é Leite
+
+==========================================================
+
+fun main (args: Array>String>) {
+
+var listaCompras = arrayOf ("Arroz", "Feijao", "Batata", "Uva", "Banana", "Carne", "Leite", "Uva")
+
+//listando todos os itens 
+
+  println(listaCompras(Arrays.toString(listaCompras))
+  
+}
+
+//resultado sao todos os itens da lista [Arroz, Feijao, Batata, Uva, Banana, Carne, Leite, Ovos]
+
+==========================================================
+//outro comando é o tamanho da lista
+
+fun main (args: Array>String>) {
+
+var listaCompras = arrayOf ("Arroz", "Feijao", "Batata", "Uva", "Banana", "Carne", "Leite", "Uva")
+
+//tamanho da lista , Se eu quiser saber o tamanho da lista
+//vou saber quantos itens tem nessa lista
+
+  println(listaCompras(listaCompras.size))
+  
+}
+
+///resultao: 9
+
+==========================================================
+
+
+
+fun main (args: Array>String>) {
+
+  //diferente do java, se adicionar numero, daria erro, ns lista com kotlin se adicionar numero, como 100, nao da erro. 
+
+var listaCompras = arrayOf ("Arroz", "Feijao", "Batata", "Uva", "Banana", "Carne", "Leite", "Uva", 100)
+
+//tamanho da lista , Se eu quiser saber o tamanho da lista
+//vou saber quantos itens tem nessa lista
+
+  println(listaCompras(9)
+  
+}
+
+///resultao: 100
+
+==========================================================
+      
+
+
+fun main (args: Array>String>) {
+
+  //diferente do java, se adicionar numero, daria erro, ns lista com kotlin se adicionar numero, como 100, nao da erro. 
+
+//var listaCompras = arrayOf ("Arroz", "Feijao", "Batata", "Uva", "Banana", "Carne", "Leite", "Uva", 100)
+var listaNumeros = arrayOf(10, 20, 30, 40, 50)
+//tamanho da lista , Se eu quiser saber o tamanho da lista
+//vou saber quantos itens tem nessa lista
+
+  println(listaCompras(2)
+
+  
+}
+
+///resultao: 30
+
+==========================================================
