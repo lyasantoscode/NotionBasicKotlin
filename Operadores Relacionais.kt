13af0b51d96bac9ca3859959ebb983b54f100a43
lyasantoscode <147869869+lyasantoscode@users.noreply.github.com>
2024-11-12 21:06:59 -0400

    Operadores Relacionais.kt

---
+++ Operadores Relacionais.kt
@@ -0,0 +1,85 @@
+fun main (args: Array<String>) {
+
+var valor1 = 20
+var valor2 = 10
+
+  
+println(valor1 == valor2)  //igual, nisso ele tem que retornar um estado booleano se é true our false
+//vai retornar false aqui
+
+}
+=============================================================
+
+fun main (args: Array<String>) {
+
+var valor1 = 20
+var valor2 = 10
+
+  
+println(valor1 < valor2)  //operador menor < 
+
+//menor, resultado que 20 nao é menor que 10, entao retorna false.
+
+}
+=============================================================
+
+fun main (args: Array<String>) {
+
+var valor1 = 20
+var valor2 = 10
+
+  
+println(valor1 > valor2)  //operador maior < 
+
+//maior, resultado que 20  é maior que 10, entao retorna true.
+
+}
+=============================================================
+fun main (args: Array<String>) {
+
+var valor1 = 20
+var valor2 = 10
+
+  
+println(valor1 <= valor2)  //operador menor igual
+
+//20 é menor igual a dez, false.
+
+//20 é igual a dez, false tambem.
+
+}
+=============================================================
+fun main (args: Array<String>) {
+
+var valor1 = 8
+var valor2 = 10
+
+  
+println(valor1 >= valor2)  //operador maior igual
+
+//8 é maior que dez, false 
+
+//8 é igual a dez, false.
+
+//entao, resulta em false
+
+}
+
+=============================================================
+fun main (args: Array<String>) {
+
+var valor1 = 20
+var valor2 = 10
+
+  
+println(valor1 != valor2)  //operador diferente
+
+//20 é diferente de 10, true.
+
+}
+
+
+
+
+
+  
