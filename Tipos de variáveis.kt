5c76a82a7192da7120f053b55647a3d371608f37
lyasantoscode <147869869+lyasantoscode@users.noreply.github.com>
2024-11-12 20:12:56 -0400

    Tipos de variáveis.kt

---
+++ Tipos de variáveis.kt
@@ -0,0 +1,20 @@
+/*
+
+String - Textos
+byte - Numeros Inteiros (-128 ate 127)
+int - Numeros inteiros (-2345678901 ate 2345678912)
+long - Numeros Inteiros (-2323232323454545678 ate 9191922299118877665)
+float - Numeros Decimais (1.2334354 ate 7 casas decimais)
+double - Numereros Decimais (1.123456789 ate 15 casas decimais)
+boolean - Valores Booleanos (true ou false)
+
+*/
+
+val name = "Marcos"
+val numeroInteiro = 2147483647
+val numeroLong = 9212121212121212121
+val dinheiro = 2.1234567
+val verdadeiro = true
+val falso = false
+
+println(falso)
